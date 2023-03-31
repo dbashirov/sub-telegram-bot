2e4ac51f255a884378feea1fa3540b2e17d35532
dbashirov <dam.bashirov@gmail.com>
2023-03-31 14:08:31 +0300

    dev

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	uptConf := tgbotapi.NewUpdate(0)
+	uptConf.Timeout = 60
+
+	updates := bot.GetUpdatesChan(uptConf)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "implement me"
+		case "say hi":
+			msg.Text = "Hi"
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
